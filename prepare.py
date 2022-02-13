26a524400885f13f767e8474ee86bcf2dc6fc8c9
Brent <brent.schriver@gmail.com>
2022-02-13 14:47:33 -0600

    Updating exploratory data analysis.

--- prepare.py
+++ prepare.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
 def prep_iris(df):
     df.drop(columns='species_id', inplace=True)
     df.rename(columns={'species_name':'species'}, inplace=True)
@@ -5,13 +8,27 @@ def prep_iris(df):
     iris = pd.concat([df, dummy_df], axis=1)
     return iris
 
+
 def prep_titanic(df):
-    columns_to_drop = ['age', 'deck', 'parch', 'embarked']
-    df.drop(columns_to_drop, inplace=True, axis=1)
-    categorical_columns = df.select_dtypes(include='object').columns
-    dummy_df = pd.get_dummies(df[categorical_columns], drop_first=True)
-    titanic = pd.concat([df, dummy_df], axis=1)
-    return titanic
+    '''
+    take in titanc dataframe, remove all rows where age or embarked is null, 
+    get dummy variables for sex and embark_town, 
+    and drop sex, deck, passenger_id, class, and embark_town. 
+    '''
+
+    df = df[(df.age.notna()) & (df.embarked.notna())]
+    df = df.drop(columns=['deck', 'passenger_id', 'class'])
+
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], prefix=['sex', 'embark'])
+
+    df = pd.concat([df, dummy_df.drop(columns=['sex_male'])], axis=1)
+
+    df = df.drop(columns=['sex', 'embark_town']) 
+
+    df = df.rename(columns={"sex_female": "is_female"})
+
+    return df
+
 
 def prep_telco(df):
     columns_to_drop = ['payment_type_id', 'internet_service_type_id', 'contract_type_id']
@@ -20,3 +37,20 @@ def prep_telco(df):
     dummy_df = pd.get_dummies(categorical_columns, drop_first=True)
     telco = pd.concat([df, dummy_df], axis=1)
     return telco
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
